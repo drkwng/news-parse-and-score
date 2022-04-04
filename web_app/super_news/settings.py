861edacf5f00e40e95755bb9f3b03608407c2ff3
drkwng <iamdrkwng@gmail.com>
2022-04-04 14:22:50 +0200

    settings and env sample

--- web_app/super_news/settings.py
+++ web_app/super_news/settings.py
@@ -82,11 +82,11 @@ WSGI_APPLICATION = 'super_news.wsgi.application'
 DATABASES = {
     'default': {
         "ENGINE": os.environ.get("SQL_ENGINE", "django.db.backends.sqlite3"),
-        "NAME": os.environ.get("POSTGRES_NAME", BASE_DIR / "db.sqlite3"),
+        "NAME": os.environ.get("POSTGRES_DB", BASE_DIR / "db.sqlite3"),
         "USER": os.environ.get("POSTGRES_USER", "user"),
         "PASSWORD": os.environ.get("POSTGRES_PASSWORD", "password"),
-        "HOST": os.environ.get("SQL_HOST", "localhost"),
-        "PORT": os.environ.get("SQL_PORT", "5432"),
+        "HOST": os.environ.get("POSTGRES_HOST", "localhost"),
+        "PORT": os.environ.get("POSTGRES_PORT", "5432"),
     }
 }
 
